5402f576da83f370d01f205e6b7bc1eeedd835f1
digitensions <pittshick@me.com>
2023-11-03 15:01:56 +0000

    Document EN 15907 script relocations

---
+++ document_en_15907/replay_json_creation.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env LANG=en_UK.UTF-8 /usr/local/bin/python3
+
+'''
+replay_json_creation.py
+
+Script function:
+1. Iterates watch folder looking for MKV files.
+2. Extracts filename, and searches in CID API manifestations
+   database for exact match in utb.content field
+3. Where found (there may be multiple matches) extract
+   part_of_reference and lref, time_code.start and end
+   and add each records data to list of dicts
+4. Convert each list into a JSON line, and add all to
+   a JSON formatted file, saved as MKV filename.json
+5. Move both the MKV and JSON to the BFI_replay folder.
+
+Joanna White
+2021
+'''
+
+import os
+import sys
+import json
+import shutil
+import logging
+import datetime
+import tenacity
+
+# Private packages
+sys.path.append(os.environ['CODE'])
+import adlib
+
+# Global variables
+WATCH_FOLDER = os.environ['BFI_REPLAY']
+SPLIT_PATH = os.path.join(WATCH_FOLDER, 'split')
+MULTI_SPLIT_PATH = os.path.join(WATCH_FOLDER, 'multiple_split')
+NO_SPLIT_PATH = os.path.join(WATCH_FOLDER, 'no_split')
+LOG_PATH = os.environ['LOG_PATH']
+CID_API = os.environ['CID_API']
+CONTROL_JSON = os.path.join(LOG_PATH, 'downtime_control.json')
+TODAY = str(datetime.datetime.now())
+TODAY_DATE = TODAY[:10]
+TODAY_TIME = TODAY[11:19]
+
+# Setup logging
+LOGGER = logging.getLogger('replay_json_creation')
+HDLR = logging.FileHandler(os.path.join(LOG_PATH, 'replay_json_creation.log'))
+FORMATTER = logging.Formatter('%(asctime)s\t%(levelname)s\t%(message)s')
+HDLR.setFormatter(FORMATTER)
+LOGGER.addHandler(HDLR)
+LOGGER.setLevel(logging.INFO)
+
+# CID config
+CID = adlib.Database(url=CID_API)
+CUR = adlib.Cursor(CID)
+
+
+def check_control():
+    '''
+    Check control json for downtime requests
+    '''
+    with open(CONTROL_JSON) as control:
+        j = json.load(control)
+        if not j['pause_scripts']:
+            LOGGER.info('Script run prevented by downtime_control.json. Script exiting.')
+            sys.exit('Script run prevented by downtime_control.json. Script exiting.')
+
+
+def cid_check():
+    '''
+    Tests if CID active before all other operations commence
+    '''
+    try:
+        CUR = adlib.Cursor(CID)
+    except KeyError:
+        LOGGER.critical('Cannot establish CID session, exiting script')
+        sys.exit()
+
+
+@tenacity.retry(stop=(tenacity.stop_after_delay(60) | tenacity.stop_after_attempt(10)))
+def cid_retrieve(filename, search):
+    '''
+    Receive filename and search in CID manifestations
+    Return list of lists to main. Retry every minute for
+    10 minutes in case of problem accessing CID API
+    '''
+    query = {'database': 'manifestations',
+             'search': search,
+             'limit': '0',
+             'output': 'json',
+             'fields': 'time_code.start, time_code.end, part_of_reference, part_of_reference.lref'}
+    try:
+        query_result = CID.get(query)
+        return query_result.records
+    except Exception as err:
+        LOGGER.exception("cid_retrieve(): Unable to retrieve data for %s\n%s", filename, err)
+        raise Exception from err
+
+    return None
+
+
+def extract_prirefs(data_list):
+    '''
+    Iterate returned CID hits for individual prirefs
+    '''
+    prirefs = []
+    for dct in data_list:
+        try:
+            priref = dct['priref'][0]
+            prirefs.append(priref)
+        except (KeyError, IndexError):
+            priref = ''
+
+    return prirefs
+
+
+def create_dictionary(cid_data):
+    '''
+    Extract data and list of dictionaries
+    '''
+    loop_dict = {}
+    for dct in cid_data:
+        try:
+            priref = dct['priref'][0]
+            print(priref)
+        except (KeyError, IndexError) as err:
+            print(err)
+        try:
+            ob_num = dct['object_number'][0]
+            print(ob_num)
+        except (KeyError, IndexError) as err:
+            print(err)
+        try:
+            parent_ob_num = dct['Part_of'][0]['part_of_reference'][0]['object_number'][0]
+        except (KeyError, IndexError) as err:
+            print(err)
+        try:
+            parent_priref = dct['Part_of'][0]['part_of_reference.lref'][0]
+        except (KeyError, IndexError) as err:
+            print(err)
+        # Check Part_of data present
+        if (len(parent_ob_num) == 0 or len(parent_priref) == 0):
+            continue
+        try:
+            time_code_start = dct['Time_code'][0]['time_code.start'][0]
+            print(time_code_start)
+        except (KeyError, IndexError) as err:
+            time_code_start = ''
+            print(err)
+        try:
+            time_code_end = dct['Time_code'][0]['time_code.end'][0]
+            print(time_code_end)
+        except (KeyError, IndexError) as err:
+            time_code_end = ''
+            print(err)
+        # Check time code data present
+        if (len(time_code_start) == 0 or len(time_code_end) == 0):
+            continue
+
+        loop_dict[f"{parent_ob_num}, {parent_priref}"] = f"{time_code_start}, {time_code_end}"
+
+    return loop_dict
+
+
+def main():
+    '''
+    Retrieve list of MKV names, match to utb in manifestation
+    Return dictionary of dictionaries, containing part_of_ref
+    and time_code start/end data for each matching manifestation.
+    Pretty print to json file, named as MKV filename. Move to replay.
+    '''
+    check_control()
+    cid_check()
+
+    LOGGER.info("------------- Replay JSON creation script START --------------")
+    files = [x for x in os.listdir(WATCH_FOLDER) if x.startswith('N_') and x.endswith(('.mkv', '.MKV'))]
+    if not len(files) > 0:
+        LOGGER.info("No files located at this time, exiting.")
+        LOGGER.info("------------- Replay JSON creation script END ----------------\n")
+        sys.exit("No files located at this time, exiting.")
+    LOGGER.info("Files located for time code extraction: %s", files)
+
+    # Iterate each item in file list extracting timecode data
+    for filename in files:
+        LOGGER.info("------------- %s -------------", filename)
+        filepath = os.path.join(WATCH_FOLDER, filename)
+
+        if not os.path.exists(filepath):
+            LOGGER.info("Skipping: File %s as it doesn't exist in watch folder", filename)
+            continue
+
+        local_logger(f"---- New file: {filename} ---------------")
+        LOGGER.info("Retrieving Priref, Timecode and Part Of data from CID...")
+        cid_data_all = cid_retrieve(filename, f"(utb.content='{filename}' WHEN utb.fieldname='bfi_replay_filename')")
+
+        if not cid_data_all:
+            local_logger(f"Problem retrieving data from CID, please check {filename} details.\n")
+            LOGGER.warning("SKIPPING: No data retrieved from CID for file %s", filename)
+            continue
+
+        # Split workflow begins
+        data = {}
+        dct_data_compiled = {}
+        prirefs = extract_prirefs(cid_data_all)
+
+        for priref in prirefs:
+            data = cid_retrieve(filename, f"priref='{priref}'")
+            dct_data = create_dictionary(data)
+            LOGGER.info("File %s priref: %s", filename, priref)
+            for k, v in dct_data.items():
+                if len(v) == 0:
+                    continue
+                dct_data_compiled[k] = v
+
+        if len(dct_data_compiled) == 0:
+            split = 'no_split'
+        if len(dct_data_compiled) == 1:
+            split = 'split'
+        if len(dct_data_compiled) > 1:
+            split = 'multiple_split'
+
+        if split == 'no_split':
+            local_logger("No splitting required for this file. Moving to no_split/ folder")
+            LOGGER.info("No splitting required. Moving to no_split/ folder.")
+            success = move_files(filepath, 'no_split')
+            if success:
+                local_logger("MKV Moved successfully.\n")
+                LOGGER.info("Moved %s to no_split path successful", filename)
+            else:
+                local_logger("FAIL MKV MOVE: Leaving in place for repeat attempt next script run.\n")
+                LOGGER.warning("%s - Failed to move to no_split/ folder. Retry next pass.", filename)
+            continue
+
+        local_logger("Making JSON file with CID data...")
+        local_logger(dct_data_compiled)
+        json_path = json_dump(dct_data_compiled, filename)
+        LOGGER.info("Data retrieved from CID: %s", dct_data_compiled)
+
+        if not os.path.exists(json_path):
+            LOGGER.warning("SKIPPING: JSON path doesn't exist: %s", json_path)
+            local_logger("FAIL: JSON file creation. Leaving MKV file in place for repeat attempt next script run.\n")
+            continue
+
+        LOGGER.info("New JSON file created: %s", json_path)
+        json_size = os.path.getsize(json_path)
+        if not json_size > 10:
+            LOGGER.warning("SKIPPING MOVE: Data not written to JSON file correctly. Deleting JSON file for retry.")
+            local_logger("FAIL: JSON file creation. Leaving MKV file in place for repeat attempt next script run.\n")
+            os.remove(json_path)
+            continue
+
+        # Moving JSON file alongside when multiple items per tape?
+        local_logger(f"JSON file created successfully, MKV and JSON being moved to {split}/ folder:")
+        LOGGER.info("Moving %s to new BFI Replay %s path", filename, split)
+        success = move_files(filepath, split)
+        if success:
+            local_logger("MKV Moved successfully.")
+            LOGGER.info("Move successful.")
+        else:
+            local_logger(f"FAIL MKV MOVE: Manual move of MKV file required to {split}/ folder.")
+            LOGGER.warning("%s - Failed to move to %s/ folder. Manual move required.", filename, split)
+
+        LOGGER.info("Moving JSON file to new BFI Replay %s path", split)
+        success2 = move_files(json_path, split)
+        if success2:
+            local_logger("JSON moved successfully.\n")
+            LOGGER.info("JSON Move successful")
+        else:
+            LOGGER.warning("FAIL JSON MOVE: Failed to move JSON for file to %s/ folder.", split)
+            local_logger(f"FAIL JSON MOVE: Manual move of JSON file required to {split}/ folder.\n")
+
+    LOGGER.info("------------- Replay JSON creation script END ----------------\n")
+
+
+def json_dump(data, filename):
+    '''
+    Split filename and make .json, dump text to json
+    '''
+
+    file = os.path.splitext(filename)[0]
+    file_json = os.path.join(WATCH_FOLDER, f"{file}.json")
+    try:
+        with open(file_json, 'x') as new_file:
+            new_file.close()
+    except FileExistsError:
+        return file_json
+
+    with open(file_json, 'w') as json_file:
+        json_file.write(json.dumps(data, indent=4))
+        json_file.close()
+
+    return file_json
+
+
+def move_files(filepath, arg):
+    '''
+    Move to split/no_split paths
+    '''
+
+    file = os.path.basename(filepath)
+
+    if arg == 'no_split':
+        new_path = os.path.join(NO_SPLIT_PATH, file)
+    elif arg == 'split':
+        new_path = os.path.join(SPLIT_PATH, file)
+    elif arg == 'multiple_split':
+        new_path = os.path.join(MULTI_SPLIT_PATH, file)
+    LOGGER.info("move_files(): Moving %s to %s", file, new_path)
+
+    try:
+        shutil.move(filepath, new_path)
+        return True
+    except Exception as err:
+        LOGGER.warning("move_files(): Move failed:\n%s", err)
+        return False
+
+
+def local_logger(data):
+    '''
+    Print local log to WATCH_FOLDER
+    '''
+
+    local_log = os.path.join(WATCH_FOLDER, 'BFI_replay_split_JSON_creation.log')
+    now = datetime.datetime.now()
+    timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    with open(local_log, 'a+') as log:
+        log.write(f"{timestamp} -- {data}\n")
+        log.close()
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ document_en_15907/yacf_rename_move.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env LANG=en_UK.UTF-8 /usr/local/bin/python3
+
+'''
+Young Audience Content Fund rename and move to autoingest:
+1. Iterate through files in Video Operations Completed/ folder
+2. Where file endswith MXF or MOV extracts filename and searches in CID's
+   `digital.acquired_filename` field for a wholename match (inc ext)
+   a. Where found: extract object number from record and create N_123456_01of01
+   (always 01of01 part whole) filename with correct file format extension
+   b. Where not found: move file to 'CID_item_not_found' folder and append log.
+   Script exits
+3. Renames files and updates logs
+4. Moves new file to Video_operations/finished/autoingest path
+
+Script must be called from Python3 in ENV for adlib.py
+
+Joanna White
+2021
+'''
+
+# Public packages
+import datetime
+import logging
+import shutil
+import json
+import sys
+import os
+
+# Private packages
+sys.path.append(os.environ['CODE'])
+import adlib
+
+# Global path variables
+YACF_PATH = os.environ['YACF_COMPLETE']
+YACF_NO_CID = os.path.join(YACF_PATH, 'CID_item_not_found')
+AUTOINGEST = os.environ['AUTOINGEST_YACF']
+LOG_PATH = os.environ['LOG_PATH']
+LOCAL_LOG = os.path.join(YACF_PATH, 'YACF_renumbering.log')
+CONTROL_JSON = os.path.join(LOG_PATH, 'downtime_control.json')
+CID_API = os.environ['CID_API']
+
+# Setup logging
+LOGGER = logging.getLogger('YACF_rename_move.log')
+HDLR = logging.FileHandler(os.path.join(LOG_PATH, 'YACF_rename_move.log'))
+FORMATTER = logging.Formatter('%(asctime)s\t%(levelname)s\t%(message)s')
+HDLR.setFormatter(FORMATTER)
+LOGGER.addHandler(HDLR)
+LOGGER.setLevel(logging.INFO)
+
+# Global variables
+CID = adlib.Database(url=CID_API)
+CUR = adlib.Cursor(CID)
+TODAY = str(datetime.datetime.now())
+TODAY_DATE = TODAY[:10]
+TODAY_TIME = TODAY[11:19]
+DATE_TIME = (f"{TODAY_DATE} = {TODAY_TIME}")
+
+
+def check_control():
+    '''
+    Check control json for downtime requests
+    '''
+    with open(CONTROL_JSON) as control:
+        j = json.load(control)
+        if not j['pause_scripts']:
+            LOGGER.info('Script run prevented by downtime_control.json. Script exiting.')
+            sys.exit('Script run prevented by downtime_control.json. Script exiting.')
+
+
+def cid_check():
+    '''
+    Tests if CID active before all other operations commence
+    '''
+    try:
+        LOGGER.info('* Initialising CID session... Script will exit if CID off line')
+        CUR = adlib.Cursor(CID)
+        LOGGER.info("* CID online, script will proceed")
+    except KeyError:
+        print("* Cannot establish CID session, exiting script")
+        LOGGER.critical('Cannot establish CID session, exiting script')
+        sys.exit()
+
+
+def cid_retrieve(filename):
+    '''
+    Receive filename and search in CID items
+    Return object number to main
+    '''
+    search = f'digital.acquired_filename="{filename}"'
+
+    query = {'database': 'items',
+             'search': search,
+             'limit': '0',
+             'output': 'json',
+             'fields': 'priref, object_number, title, title.article'}
+    try:
+        query_result = CID.get(query)
+        LOGGER.info("cid_retrieve(): Making CID query request with:\n %s", query)
+    except Exception:
+        print(f"cid_retrieve(): Unable to retrieve data for {filename}")
+        LOGGER.exception("cid_retrieve(): Unable to retrieve data for %s", filename)
+        query_result = None
+
+    try:
+        priref = query_result.records[0]['priref'][0]
+    except (KeyError, IndexError) as err:
+        priref = ""
+        LOGGER.warning("cid_retrieve(): Unable to access priref %s", err)
+    try:
+        ob_num = query_result.records[0]['object_number'][0]
+    except (KeyError, IndexError) as err:
+        ob_num = ""
+        LOGGER.warning("cid_retrieve(): Unable to access object_number: %s", err)
+    try:
+        title = query_result.records[0]['Title'][0]['title'][0]
+    except (KeyError, IndexError) as err:
+        title = ""
+        LOGGER.warning("cid_retrieve(): Unable to access title: %s", err)
+    try:
+        title_article = query_result.records[0]['Title'][0]['title.article'][0]
+    except (KeyError, IndexError) as err:
+        title_article = ""
+        LOGGER.warning("cid_retrieve(): Unable to access title article %s", err)
+
+    return (priref, ob_num, title, title_article)
+
+
+def main():
+    '''
+    search in CID Item for digital.acquired_filename
+    Retrieve object number and use to build new filename for YACF file
+    Update local log for YACF monitoring
+    Move file to autoingest path
+    '''
+    LOGGER.info("=========== YACF script start ==========")
+    check_control()
+    cid_check()
+    for root, dirs, files in os.walk(YACF_PATH):
+        for file in files:
+            filepath = os.path.join(root, file)
+            if 'CID_item_not_found' in filepath:
+                LOGGER.info("WRONG PATH: Skipping %s as item located in 'CID_item_not_found' folder", file)
+                local_logger(f"\nWRONG PATH: Skipping file {file}, as located in 'CID_item_not_found' folder")
+                continue
+            if file.endswith(('.MXF', '.mxf', '.MOV', '.mov')):
+                LOGGER.info("----------------- New file found %s ----------------", file)
+                LOGGER.info("Processing %s now. Looking in CID item records for filename", file)
+                cid_data = cid_retrieve(file)
+                priref = cid_data[0]
+                ob_num = cid_data[1]
+                title = cid_data[2]
+                title_art = cid_data[3]
+                if len(ob_num) > 0:
+                    LOGGER.info("CID item data retrieved - Priref: %s  Object_number: %s  Title: %s %s", priref, ob_num, title_art, title)
+                    local_logger("\n------------------------- New file found ------------------------------")
+                    local_logger(f"Processing file: {file}. Retrieving CID item record data")
+                    local_logger(f"Data retrieved from CID Item:\nItem object number: {ob_num}\nTitle: {title_art} {title}")
+                    local_logger(f"** Renumbering file {file} with object number {ob_num}")
+                    new_filepath, new_file = rename(filepath, ob_num)
+                    if len(new_file) > 0:
+                        local_logger(f"New filename generated: {new_file}")
+                        local_logger(f"File renumbered and filepath updated to: {new_filepath}")
+                        success = move(new_filepath, 'ingest')
+                        if success:
+                            local_logger(f"File {new_file} relocated to Autoingest {DATE_TIME}")
+                            local_logger("---------------- File process complete ----------------")
+                        else:
+                            LOGGER.warning("FILE %s DID NOT MOVE SUCCESSFULLY TO AUTOINGEST", new_file)
+                            local_logger(f"ERROR MOVING FILE: Script could not move file to Autoingest. Please check file permissions. {DATE_TIME}")
+                    else:
+                        LOGGER.warning("Problem creating new number for %s", file)
+                        local_logger(f"ERROR RENAMING FILE: Problem found when attempting to rename file {file}")
+                        local_logger("ERROR RENAMING FILE: Please check file has no permissions limitations, script will retry later")
+                else:
+                    LOGGER.info("File information not found in CID. Moving file to 'CID_item_not_found' folder")
+                    local_logger(f"\nNO CID DATA: File found {file} but no CID data retrieved")
+                    local_logger("NO CID DATA: Moving file to 'CID_item_not_found' folder - please check filename and CID item record")
+                    success2 = move(filepath, 'fail')
+                    if success2:
+                        LOGGER.info("File moved successfully")
+                        local_logger(f"NO CID DATA: File {file} relocated to 'CID_item_not_found' folder {DATE_TIME}")
+                    else:
+                        LOGGER.warning("FILE %s DID NOT MOVE SUCCESSFULLY TO FOLDER", file)
+                        local_logger(f"ERROR MOVING FILE: Script could not move file. Please check file permissions. Script will retry. {DATE_TIME}")
+            else:
+                LOGGER.info("Skipping. File found that is not MXF or MOV: %s", file)
+
+
+def rename(filepath, ob_num):
+    '''
+    Receive original file path and rename filename
+    based on object number, return new filepath, filename
+    '''
+    new_filepath, new_filename = '', ''
+    path, filename = os.path.split(filepath)
+    ext = os.path.splitext(filename)
+    new_name = ob_num.replace('-', '_')
+    new_filename = f"{new_name}_01of01{ext[1]}"
+    print(f"Renaming {filename} to {new_filename}")
+    new_filepath = os.path.join(path, new_filename)
+
+    try:
+        os.rename(filepath, new_filepath)
+    except OSError:
+        LOGGER.warning("There was an error renaming %s to %s", filename, new_filename)
+
+    return (new_filepath, new_filename)
+
+
+def move(filepath, arg):
+    '''
+    Move existing filepaths to Autoingest
+    '''
+    if os.path.exists(filepath) and 'fail' in arg:
+        print(f"move(): Moving {filepath} to {YACF_NO_CID}")
+        try:
+            shutil.move(filepath, YACF_NO_CID)
+            return True
+        except Exception as err:
+            LOGGER.warning("Error trying to move file %s to %s. Error: %s", filepath, YACF_NO_CID, err)
+            return False
+    elif os.path.exists(filepath) and 'ingest' in arg:
+        print(f"move(): Moving {filepath} to {AUTOINGEST}")
+        try:
+            shutil.move(filepath, AUTOINGEST)
+            return True
+        except Exception:
+            LOGGER.warning("Error trying to move file %s to %s", filepath, AUTOINGEST)
+            return False
+    else:
+        return False
+
+
+def local_logger(data):
+    '''
+    Pretty printed log for human readable data
+    Output local log data for Video Ops teams to monitor renaming process
+    '''
+    if len(data) > 0:
+        with open(LOCAL_LOG, 'a+') as log:
+            log.write(data + "\n")
+            log.close()
+
+
+if __name__ == '__main__':
+    main()

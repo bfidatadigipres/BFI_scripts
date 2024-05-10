0a81d4799fda68a659382313e7b96e956e99a854
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-05-10 12:32:06 +0100

    Remove print from adlib_v3

--- adlib_v3.py
+++ adlib_v3.py
@@ -101,7 +101,6 @@ def post(api, payload, database, method):
         except (requests.Timeout, requests.ConnectionError, requests.HTTPError) as err:
             print(response.text, err)
             return None
-        print(response.text)
         if 'recordList' in response.text:
             records = json.loads(response.text)
             if isinstance(records['adlibJSON']['recordList']['record'], list):                 

5bb325d9dc4fb2fdf4879144b449b2d33479878f
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-05-20 12:04:45 +0100

    Clean up '@lang' check

--- adlib_v3.py
+++ adlib_v3.py
@@ -137,7 +137,7 @@ def retrieve_field_name(record, fieldname):
         for field in record[f'{fieldname}']:
             if isinstance(field, str):
                 field_list.append(field)
-            elif '@lang' in str(field) or 'lang' in str(field):
+            elif "'@lang'" in str(field):
                 field_list.append(field['value'][0]['spans'][0]['text'])
             else:
                 field_list.append(field['spans'][0]['text'])

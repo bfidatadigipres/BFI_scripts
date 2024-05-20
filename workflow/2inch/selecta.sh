839bab1351d336ea5015e7ee24cf083f961ed89f
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-05-20 17:08:01 +0100

    Log correction in start script

--- workflow/2inch/selecta.sh
+++ workflow/2inch/selecta.sh
@@ -5,7 +5,7 @@
 # scope records in a pointerfile
 
 # Log script start
-echo "Start Selecta: $(date)" >> "${CODE}workflow/2inch/selecta.log"
+echo "Start Selecta: $(date)" >> "${LOG_PATH}2inch_selecta.log"
 
 # Collect selections from pointer file
 "$PYENV311" "${CODE}workflow/2inch/selecta.py"
@@ -14,4 +14,4 @@ echo "Start Selecta: $(date)" >> "${CODE}workflow/2inch/selecta.log"
 "$PYENV311" "${CODE}workflow/2inch/submitta.py"
 
 # Log script end
-echo "Finish Selecta: $(date)" >> "${CODE}workflow/2inch/selecta.log"
+echo "Finish Selecta: $(date)" >> "${LOG_PATH}2inch_selecta.log"

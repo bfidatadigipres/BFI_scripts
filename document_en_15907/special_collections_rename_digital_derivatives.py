d17ab390cbb658fe62f3fc3943c77773cc494eb7
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-07-30 16:17:23 +0100

    Spelling error in foldername

--- document_en_15907/special_collections_rename_digital_derivatives.py
+++ document_en_15907/special_collections_rename_digital_derivatives.py
@@ -41,7 +41,7 @@ import utils
 
 # Global path variables
 SCPATH = os.environ['SPECIAL_COLLECTIONS']
-STORAGE = os.path.join(SCPATH, 'Uncatalogued_stills_digital_derivative/')
+STORAGE = os.path.join(SCPATH, 'Uncatalogued_stills_digitial_derivative/')
 AUTOINGEST = os.path.join(os.environ['AUTOINGEST_IS_SPEC'], 'ingest/proxy/image/')
 LOG = os.path.join(os.environ['LOG_PATH'], 'special_collections_rename_digital_derivatives.log')
 MEDIAINFO_PATH = os.path.join(os.environ['LOG_PATH'], 'cid_mediainfo/')

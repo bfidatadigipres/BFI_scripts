73f738e4c6043538e75e7d84e45cfbe3b6cae614
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-08-22 10:16:15 +0100

    Print xml

--- black_pearl/black_pearl_validate_make_record_targeted.py
+++ black_pearl/black_pearl_validate_make_record_targeted.py
@@ -544,7 +544,7 @@ def create_media_record(ob_num, duration, byte_size, filename, bucket, session):
                     {'preservation_bucket': bucket}])
     print(record_data)
     record_data_xml = adlib.create_record_data(CID_API, 'media', session, '', record_data)
-    print(record_data_xml)
+    logger.info(record_data_xml)
 
     try:
         item_rec = adlib.post(CID_API, record_data_xml, 'media', 'insertrecord', session)

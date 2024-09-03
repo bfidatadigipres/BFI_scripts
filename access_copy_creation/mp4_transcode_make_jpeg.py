f75685d571a9722664157036362030342cf779ea
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-09-03 09:54:55 +0100

    Force aac to audio encodings, don't rely on FFmpeg defaults

--- access_copy_creation/mp4_transcode_make_jpeg.py
+++ access_copy_creation/mp4_transcode_make_jpeg.py
@@ -889,20 +889,20 @@ def create_transcode(fullpath, output_path, height, width, dar, par, audio, defa
         ]
     elif fl_fr is True:
         map_audio = [
-            "-map", "0:a?",
+            "-map", "0:a?", "-c:a", "aac",
             "-ac", "2", "-dn"
         ] 
     elif default and audio:
         print(f"Default {default}, Audio {audio}")
         map_audio = [
-            "-map", "0:a?",
+            "-map", "0:a?", "-c:a", "aac",
             f"-disposition:a:{default}",
             "default", "-dn"
         ]       
     else:
         map_audio = [
             "-map", "0:a?",
-            "-dn"
+             "-c:a", "aac", "-dn"
         ]
 
     height = int(height)

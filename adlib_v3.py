858bc89dccc215e0d0e44faea3160f66bec76941
Joanna White <37188631+digitensions@users.noreply.github.com>
2024-12-11 09:21:01 +0000

    Recycle prompt for code

--- adlib_v3.py
+++ adlib_v3.py
@@ -422,3 +422,12 @@ def add_quality_comments(api, priref, comments):
         return False
     else:
         return True
+
+
+def recycle_api(api):
+    '''
+    Adds a search call to API which
+    triggers Powershell recycle
+    '''
+    search = 'title=recycle.application.pool.data.tes'
+    return get(api, search)
--- adlib_v3_sess.py
+++ adlib_v3_sess.py
@@ -437,3 +437,11 @@ def add_quality_comments(api, priref, comments, session=None):
     else:
         return True
 
+
+def recycle_api(api):
+    '''
+    Adds a search call to API which
+    triggers Powershell recycle
+    '''
+    search = 'title=recycle.application.pool.data.tes'
+    return get(api, search)

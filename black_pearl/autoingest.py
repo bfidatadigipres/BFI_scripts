073ae9c6da3890a4585677e0e6a6b6dee9fe8ec6
Joanna White <37188631+digitensions@users.noreply.github.com>
2025-01-08 15:07:22 +0000

    Accept TAR for QNAP-06/03

--- black_pearl/autoingest.py
+++ black_pearl/autoingest.py
@@ -152,11 +152,11 @@ def check_accepted_file_type(fpath):
     Retrieve codec and ensure file is accepted type
     TAR accepted from DMS / ProRes all other paths
     '''
-    if any(x in fpath for x in ['qnap_access_renditions', 'qnap_10']):
-        if fpath.endswith(('tar', 'TAR')):
+    if any(x in fpath for x in ['qnap_11', 'qnap_10']):
+        if fpath.endswith(('.tar', '.TAR')):
             return True
-    if any(x in fpath for x in ['qnap_06', 'film_operations', 'qnap_film']):
-        if fpath.endswith(('mkv', 'MKV')):
+    if any(x in fpath for x in ['qnap_06', 'qnap_03']):
+        if fpath.endswith(('.mkv', '.MKV', '.tar', '.TAR')):
             return True
     formt = utils.get_metadata('Video', 'Format', fpath)
     print(f"utils.get_metadata: {formt}")
@@ -707,7 +707,7 @@ def main():
                 print(f"utils.get_size: {size}")
                 print('\t* file has not been ingested, so moving it into Black Pearl ingest folder...')
                 if int(size) > 1099511627776:
-                    logger.info('%s\tFile is larger than 1TB. Checking file is ProRes', log_paths)
+                    logger.info('%s\tFile is larger than 1TB. Checking file is ProRes or TAR', log_paths)
                     accepted_file_type = check_accepted_file_type(fpath)
                     if accepted_file_type is True:
                         try:

9f1eeb81b97f4c489f0db26bb6008e4a07ebe5a8
Joanna White <37188631+digitensions@users.noreply.github.com>
2025-04-17 11:59:19 +0100

    Temp disable check_control

--- document_en_15907/special_collections_document_archiving_osh.py
+++ document_en_15907/special_collections_document_archiving_osh.py
@@ -262,10 +262,11 @@ def main():
     Iterate supplied folder, find image files in folders
     named after work and create analogue/digital item records
     for every photo. Clean up empty folders.
-    '''
+
     if not utils.check_control('power_off_all'):
         LOGGER.info("Script run prevented by downtime_control.json. Script exiting.")
         sys.exit("Script run prevented by downtime_control.json. Script exiting.")
+    '''
     if not utils.cid_check(CID_API):
         sys.exit("* Cannot establish CID session, exiting script")
 

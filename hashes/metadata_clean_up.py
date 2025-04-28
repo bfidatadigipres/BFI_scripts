3818de4430753d3b2548f5b2067f7c400cd22497
Joanna White <37188631+digitensions@users.noreply.github.com>
2025-04-28 12:21:03 +0100

    Handle audio_codec_list return (first selection)

--- hashes/metadata_clean_up.py
+++ hashes/metadata_clean_up.py
@@ -6,7 +6,7 @@ Script to clean up Mediainfo files that belong to filepaths deleted by autoinges
 writes mediainfo data to CID media record priref before deleting files.
 
 1. Receive filepath of '*_TEXT.txt' file from sys.argv
-2. Extract filename, and create paths for all metadata possibilities
+2. Extract filename, and create paths for all metadata possibilities 
 3. Check CID Media record exists with imagen.media.original_filename matching filename
    There is no danger deleting before asset in autoingest, as validation occurs
    before CID media record creation now.
@@ -55,7 +55,7 @@ FIELDS = [
     {'container.file_size.total_gigabytes': ['FileSize_String4', 'File size  ']},
     {'container.commercial_name': ['Format_Commercial', 'Commercial name  ']},
     {'container.format': ['Format', 'Format  ']},
-    {'container.audio_codecs': ['Audio_Codec_List', 'Audio codecs ']},
+    {'container.audio_codecs': ['Audio_Codec_List', 'Audio codecs ']}, # Returns ' / ' separated list
     {'container.audio_stream_count': ['AudioCount', 'Count of audio streams  ']},
     {'container.video_stream_count': ['VideoCount', 'Count of video streams  ']},
     {'container.format_profile': ['Format_Profile','Format profile  ']},
@@ -91,13 +91,13 @@ FIELDS = [
     {'video.width': ['Width', 'Width  ']},
     {'video.format_profile': ['Format_Profile', 'Format profile  ']},
     {'video.width_aperture': ['Width_CleanAperture', 'Width clean aperture  ']}, # Guessed second
-    {'video.delay': ['Delay','Delay  ']},
+    {'video.delay': ['Delay', 'Delay  ']},
     {'video.format_settings_GOP': ['Format_Settings_GOP', 'Format settings, GOP  ']},
-    {'video.codec_id': ['CodecID','Codec ID  ']},
-    {'video.colour_space': ['ColorSpace','Color space  ']},
+    {'video.codec_id': ['CodecID', 'Codec ID  ']},
+    {'video.colour_space': ['ColorSpace', 'Color space  ']},
     {'video.colour_primaries': ['colour_primaries', 'Color primaries  ']},
     {'video.commercial_name': ['Format_Commercial', 'Commercial name  ']},
-    {'video.display_aspect_ratio': ['DisplayAspectRatio','Display aspect ratio  ']},
+    {'video.display_aspect_ratio': ['DisplayAspectRatio', 'Display aspect ratio  ']},
     {'video.format': ['Format', 'Format  ']},
     {'video.matrix_coefficients': ['matrix_coefficients', 'Matrix coefficients  ']},
     {'video.pixel_aspect_ratio': ['PixelAspectRatio', 'Pixel aspect ratio  ']},
@@ -432,6 +432,9 @@ def build_metadata_text_xml(text_path: str, text_full_path: str, priref: str) ->
                 seconds = f"{float(milliseconds) / 1000:.9f}"
                 print(f"*** Converting float milliseconds {milliseconds} into seconds {seconds} ***")
                 gen.append({f'{key}': float(seconds)})
+            if 'container.audio_codecs' in key:
+                # Handle multiples
+                pass
             if key.startswith('container.'):
                 match = iterate_text_rows(gen_rows, val[1], key)
                 if match is None:
@@ -547,6 +550,8 @@ def manipulate_data(key: str, selection: Optional[str]) -> Optional[str]:
     if selection is None:
         selection = ''
     if '.format' in key and ' / ' in selection:
+        return selection.split(' / ')[0].strip()
+    if '.audio_codecs' in key and ' / ' in selection:
         return selection.split(' / ')[0].strip()
     if '.codec_id' in key and ' / ' in selection:
         return selection.split(' / ')[0].strip()

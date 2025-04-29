a79fc2da5a558851fcbc71cc7ff15916a786ffea
digitensions <pittschick@me.com>
2025-04-29 14:36:07 +0100

    Update API data

--- splitting_scripts/delete_post_split_qnap01.py
+++ splitting_scripts/delete_post_split_qnap01.py
@@ -36,7 +36,7 @@ import models
 LOG_PATH = os.environ.get('LOG_PATH')
 LOG = os.path.join(LOG_PATH, 'delete_post_split_qnap01.log')
 CONTROL_JSON = os.path.join(LOG_PATH, 'downtime_control.json')
-CID_API = os.environ['CID_API4']
+CID_API = os.environ['CID_API3']
 CLIENT = ds3.createClientFromEnv()
 
 TARGETS: Final = [f"{os.environ['QNAP_VID']}/processing/"]

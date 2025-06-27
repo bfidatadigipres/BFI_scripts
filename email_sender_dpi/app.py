3db7cdb3bc04269e402e24b3ee0af9ea05dfeb46
SaabriinMo <saabriinm123@gmail.com>
2025-06-27 17:50:10 +0100

    adding error page

--- email_sender_dpi/app.py
+++ email_sender_dpi/app.py
@@ -1,11 +1,19 @@
+import logging
+
 from flask import Flask, flash, redirect, render_template, request, url_for
 
 import utils
 
 app = Flask(__name__, template_folder="templates")
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)-8s %(name)-15s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16 MB max file size
-app.secret_key = "pass"
+app.secret_key = "********"
 
 
 @app.route("/", methods=["GET"])
@@ -30,23 +38,31 @@ def send_screenshot():
             image_path = request.form.get("file")
 
             if email[-10:] != "bfi.org.uk":
+                logging.error("Invalid email, please enter valid email!")
                 raise ValueError("Invalid email, please enter valid email!")
 
             if not image_path:
+                logging.error("User did not add an image at all!")
                 raise ValueError("filepath is required")
+
+            if "bp_nas" not in image_path:
+                # app.logger.critical()
+                logging.error("User has added a path to an images that not in bp_nas")
+                raise ValueError("Image cant be found please move image to bp nas.")
 
             utils.send_email(email, subject, body, image_path)
 
+            logging.info(f"Email successfully sent to {email}", "success")
             flash(f"Email successfully sent to {email}", "success")
 
             return redirect(url_for("send_screenshot"))
 
         except Exception as e:
-            # Flash error message
+            logging.critical(f"Email not sent due to reason: {str(e)}")
             flash(f"Email not sent due to reason: {str(e)}", "error")
 
-            # Redirect to refresh the page
-            return redirect(url_for("send_screenshot"))
+            return render_template("error_page.html")
+
     return render_template("index.html")
 
 

aef414a05de9002e38563b6712786672594090f2
SaabriinMo <saabriinm123@gmail.com>
2025-06-30 10:17:35 +0100

    adding logging

--- email_sender_dpi/app.py
+++ email_sender_dpi/app.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 from flask import Flask, flash, redirect, render_template, request, url_for
 
@@ -6,30 +7,25 @@ import utils
 
 app = Flask(__name__, template_folder="templates")
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(levelname)-8s %(name)-15s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+logger = logging.getLogger("flask_logger")
+hdlr = logging.FileHandler("/Users/mohameds/email_temp/app.log")
+formatter = logging.Formatter("%(asctime)s\t%(levelname)s\t%(message)s")
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
 
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16 MB max file size
-app.secret_key = "********"
+app.secret_key = "*********"
 
 
 @app.route("/", methods=["GET"])
 def index():
-    """
-    returns the rendered page of index.html
-    """
     return render_template("index.html")
 
 
 @app.route("/", methods=["POST", "GET"])
 def send_screenshot():
-    """
-    process form and make the request to send the email
-    with the information or send an error.
-    """
     if request.method == "POST":
         try:
             email = request.form.get("email")
@@ -38,31 +34,30 @@ def send_screenshot():
             image_path = request.form.get("file")
 
             if email[-10:] != "bfi.org.uk":
-                logging.error("Invalid email, please enter valid email!")
+                logger.error("Invalid email, please enter valid email!")
                 raise ValueError("Invalid email, please enter valid email!")
 
             if not image_path:
-                logging.error("User did not add an image at all!")
+                logger.error("User did not add an image at all!")
                 raise ValueError("filepath is required")
 
             if "bp_nas" not in image_path:
                 # app.logger.critical()
-                logging.error("User has added a path to an images that not in bp_nas")
+                logger.error("User has added a path to an images that not in bp_nas")
                 raise ValueError("Image cant be found please move image to bp nas.")
 
             utils.send_email(email, subject, body, image_path)
 
-            logging.info(f"Email successfully sent to {email}", "success")
-            flash(f"Email successfully sent to {email}", "success")
-
-            return redirect(url_for("send_screenshot"))
+            logger.info(f"Email successfully sent to {email}", "success")
+            flash(
+                f"Email successfully sent to {email} with subject {subject}", "success"
+            )
 
         except Exception as e:
-            logging.critical(f"Email not sent due to reason: {str(e)}")
+            logger.critical(f"Email not sent due to reason: {str(e)}")
             flash(f"Email not sent due to reason: {str(e)}", "error")
 
             return render_template("error_page.html")
-
     return render_template("index.html")
 
 

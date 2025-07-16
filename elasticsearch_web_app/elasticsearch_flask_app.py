040730526db42d2a1a740417a33dcbd8c047c0dc
SaabriinMo <saabriinm123@gmail.com>
2025-07-16 10:46:32 +0100

    elasticsearch app using flask and streamlit

---
+++ elasticsearch_web_app/elasticsearch_flask_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
+from functools import lru_cache
+import elasticsearch_helper as elastic_helper
+import pandas as pd
+import os
+
+app = Flask(__name__, template_folder='templates/')
+
+ELASTIC_PASS = os.environ['ELASTIC_PASS']
+ELASTIC_PATH = os.environ['ELASTIC_PATH']
+
+es = Elasticsearch(ELASTIC_PATH,
+    basic_auth=("elastic", ELASTIC_PASS),
+    verify_certs=False)
+
+autoingest_index = []
+
+
+
+@app.route('/')
+def home():
+    filename = request.args.get("query")
+    autoingest_index = elastic_helper.get_autoingest_docs(es)
+    df = elastic_helper.search_by_filepath(filename, autoingest_index, es)
+    table_html = df.to_html(classes='table table-striped', index=False)
+    return render_template('index.html', table_html=table_html)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=9000)
+#N_10119416_01of01.ts 
---
+++ elasticsearch_web_app/streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from streamlit_dynamic_filters import DynamicFilters
+import pandas as pd
+from elasticsearch import Elasticsearch
+import elasticsearch_helper as elastic_helper
+import os
+
+ELASTIC_PASS = os.environ['ELASTIC_PASS']
+ELASTIC_PATH = os.environ['ELASTIC_PATH']
+
+st.set_page_config(page_title='Filename Search Engine', page_icon='', layout='wide')
+text_search = st.text_input("Search Filename", value='')
+
+# getting the filename
+es = Elasticsearch(ELASTIC_PATH,
+    basic_auth=("elastic", elastic_helper.ELASTIC_PASS),
+    verify_certs=False)
+
+autoingest_index =[]
+
+autoingest_index = elastic_helper.get_autoingest_docs(es)
+df = elastic_helper.search_by_filepath(text_search, autoingest_index, es)
+
+
+with st.sidebar:
+    st.write('Apply filter: ')
+
+    filter_column = ['log_level']
+    dynamic_filter= DynamicFilters(df, filters=filter_column)
+
+    dynamic_filter.display_filters(location='sidebar')
+    filtered_df = dynamic_filter.filter_df()
+if text_search and not df.empty:
+    st.write('results: ')
+    st.dataframe(filtered_df)
+else:
+    st.write('no results are found!!!')

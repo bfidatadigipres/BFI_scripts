f3429c11f6edcd2b8af2520a2d91513ff7d85e63
SaabriinMo <saabriinm123@gmail.com>
2025-08-20 15:57:00 +0100

    fix: fix test function based on pylint report.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -8,12 +8,32 @@ from helpers import stora_helper
     [
         ("25", "02:25:00", False, "02:50:00"),
         ("m", "02:25:00", True, ValueError),
-        ("25", "28:15:00", True, ValueError)("5", "0:15:0", False, "00:20:00"),
+        ("25", "28:15:00", True, ValueError),
+        ("5", "0:15:0", False, "00:20:00"),
     ],
 )
 def test_calculate_transmission_stoptime(
     duration_input, start_time_input, raise_error, expected_output
 ):
+    """
+    Test 'calculate_transmission_stoptime function' from stora_helper.py
+
+    This test validates that the function correctly computesthe transmission stop time
+    based on the given start time and durations. It also verifies that the approiate 
+    exceptions are raised for invalid input formates or out-of-range values.
+
+    Parameters:
+    -----------
+    duration_input: str
+        duration in minutes
+    start_time_input: str
+        start time in 'HH:MM:SS'
+    raise_error: bool
+        flag whether the test expect an exception.
+    expected_output: str | None
+        Expected stop time (as a string) or the expected
+        exception type if 'raise_error' is True.
+    """
 
     if raise_error:
         with pytest.raises(expected_output):

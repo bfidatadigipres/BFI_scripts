f67ee323eb6e4a408ee34aea70e03cd6d94db1de
Joanna White <37188631+digitensions@users.noreply.github.com>
2025-10-06 17:10:43 +0100

    Fix adjust seconds func

--- access_copy_creation/mp4_transcode_make_jpeg_2.py
+++ access_copy_creation/mp4_transcode_make_jpeg_2.py
@@ -749,6 +749,7 @@ def get_duration(fullpath: str) -> Optional[tuple[Union[str, int], str]]:
     """
 
     duration = utils.get_metadata("Video", "Duration", fullpath)
+    LOGGER.warning("**** DURATION: %s", duration)
     if not duration:
         return ("", "")
 

b31f2c6e0e578474628271d16eaf39f0b83a7346
digitensions <pittschick@me.com>
2026-02-25 14:11:44 +0000

    Updates to code for future work2

--- born_digital_workflows/archivematica_sip_utils.py
+++ born_digital_workflows/archivematica_sip_utils.py
@@ -812,7 +812,7 @@ def download_aip(aip_uuid: str, dpath: str, fn: str) -> Optional[str]:
             download_path = os.path.join(dpath, fname)
 
             with open(download_path, "wb") as file:
-                for chunk in response.iter_content(chunk=8192):
+                for chunk in response.iter_content(8192):
                     if chunk:
                         file.write(chunk)
             if os.path.isfile(download_path):
@@ -820,3 +820,7 @@ def download_aip(aip_uuid: str, dpath: str, fn: str) -> Optional[str]:
     except requests.exceptions.RequestException as err:
         print(err)
         return None
+
+
+def download_access_file(fname: str) -> Optional[str]:
+    pass
